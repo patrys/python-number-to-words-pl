eb7aa844c7319dfa98cadb3d342859b42d750182
Patryk Zawadzki <patrys@pld-linux.org>
2009-10-18 22:18:24 +0200

    Added license, fixed URI in README

--- to_words_pl.py
+++ to_words_pl.py
@@ -1,5 +1,23 @@
 # - coding: utf-8 -
 
+#    python-number-to-words
+#    Copyright (C) 2009  Patryk Zawadzki <patrys@pld-linux.org>
+#    Copyright (C) 2009  Mirumee Software (http://mirumee.com/)
+
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
 SUFFIXES = [
     None,
     u'jeden', u'dwa', u'trzy', u'cztery', u'pięć', u'sześć', u'siedem',
